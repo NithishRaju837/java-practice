46cef96b0d4e67fff8bcfc4897f37e2f74195d9a
NithishRaju837 <nithishrajuch11@gmail.com>
2026-07-29 19:06:27 +0530

    second commit

---
+++ Student1.java
@@ -0,0 +1,21 @@
+package com.javafundamentals;
+
+public class Student1.java{
+    public static void main(String[] args) {
+        int money = 100;
+        int pencilCost = 7;
+
+        int pencils = money / pencilCost;
+        int amountLeft = money % pencilCost;
+
+        System.out.println("Money Available: " + money + " Rupees");
+        System.out.println("Cost of One Pencil: " + pencilCost + " Rupees");
+        System.out.println("Number of Pencils Bought: " + pencils);
+        System.out.println("Amount Left After Shopping: " + amountLeft + " Rupees");
+    }
+}
+	
+		
+		
+
+
